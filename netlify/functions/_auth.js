c557951283fb27fdf5803ee2d27daf21829fb1dd
chorfla-hash <chorflachorfla9@gmail.com>
2025-09-06 21:15:55 +0400

    Update _auth.js

--- netlify/functions/_auth.js
+++ netlify/functions/_auth.js
@@ -1,1 +1,22 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { User } = require('./_models');
 
+
+function sign(user) {
+return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '7d' });
+}
+
+
+async function hashPassword(plain) {
+const salt = await bcrypt.genSalt(10);
+return bcrypt.hash(plain, salt);
+}
+
+
+async function verifyPassword(plain, hash) {
+return bcrypt.compare(plain, hash);
+}
+
+
+module.exports = { sign, hashPassword, verifyPassword };
