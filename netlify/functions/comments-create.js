70fbebfd2f850731dad7707c03380b71c9889b45
chorfla-hash <chorflachorfla9@gmail.com>
2025-09-06 21:23:26 +0400

    Update comments-create.js

--- netlify/functions/comments-create.js
+++ netlify/functions/comments-create.js
@@ -1,1 +1,25 @@
+const { connect } = require('./_db');
+const { Comment } = require('./_models');
+const { requireUser } = require('./_auth');
 
+
+exports.handler = async (event) => {
+await connect();
+const user = await requireUser(event);
+const { postId, body, parentId } = JSON.parse(event.body || '{}');
+
+
+if (!postId || !body) return { statusCode: 400, body: 'postId and body required' };
+
+
+const comment = await Comment.create({
+post: postId,
+body,
+author: user._id,
+parent: parentId || null
+});
+
+
+await comment.populate('author', 'name avatarUrl');
+return { statusCode: 200, body: JSON.stringify(comment) };
+};
