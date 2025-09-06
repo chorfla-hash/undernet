e84602612073d3697dd04f5330ca40b7a0eabdfd
chorfla-hash <chorflachorfla9@gmail.com>
2025-09-06 21:33:00 +0400

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,1 +1,26 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Feed from './pages/Feed'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import CreatePost from './pages/CreatePost'
+import PostView from './pages/PostView'
+import Profile from './pages/Profile'
 
+export default function App() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <Navbar />
+      <main className="max-w-3xl mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostView />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
