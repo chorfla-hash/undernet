ffa4a6a0da05b5523ef64d89d50a6ee6272a6670
chorfla-hash <chorflachorfla9@gmail.com>
2025-09-06 21:15:23 +0400

    Update _db.js

--- netlify/functions/_db.js
+++ netlify/functions/_db.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
 
+
+let conn = null;
+
+
+async function connect() {
+if (conn) return conn;
+const uri = process.env.MONGODB_URI;
+if (!uri) throw new Error('MONGODB_URI missing');
+conn = await mongoose.connect(uri, {
+serverSelectionTimeoutMS: 5000,
+});
+return conn;
+}
+
+
+module.exports = { connect };
