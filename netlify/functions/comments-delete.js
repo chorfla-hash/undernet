4c4fc12b78b63e850f2c94a96f8f3ce677df4f98
chorfla-hash <chorflachorfla9@gmail.com>
2025-09-06 21:30:38 +0400

    Update comments-delete.js

--- netlify/functions/comments-delete.js
+++ netlify/functions/comments-delete.js
@@ -1,1 +1,20 @@
+const { connect } = require('./_db')
+const { Comment } = require('./_models')
+const { requireUser } = require('./_auth')
 
+exports.handler = async (event) => {
+  await connect()
+  const user = await requireUser(event)
+  const { id } = event.queryStringParameters || {}
+  if (!id) return { statusCode: 400, body: 'id required' }
+
+  const comment = await Comment.findById(id)
+  if (!comment) return { statusCode: 404, body: 'Not found' }
+
+  if (String(comment.author) !== user._id.toString() && !user.isAdmin) {
+    return { statusCode: 403, body: 'Forbidden' }
+  }
+
+  await comment.deleteOne()
+  return { statusCode: 200, body: JSON.stringify({ success: true }) }
+}
