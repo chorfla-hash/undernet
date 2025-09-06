f1aadcba37f063efcfc5ed202907bcf669605def
chorfla-hash <chorflachorfla9@gmail.com>
2025-09-06 21:33:09 +0400

    Update api.js

--- web/src/api.js
+++ web/src/api.js
@@ -1,1 +1,15 @@
+import axios from 'axios'
 
+// automatically point to Netlify Functions
+const api = axios.create({
+  baseURL: '/.netlify/functions',
+})
+
+// attach token if logged in
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
+export default api
